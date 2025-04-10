def35ff1a2b570d036e10800fdbfc188e8658e2b
Gil Pasi <gilp@semperis.com>
2025-04-10 19:18:34 +0300

    [add]: Question 1's solution added

--- plp_assignment1.rkt
+++ plp_assignment1.rkt
@@ -4,4 +4,19 @@
 ;1.a
 ;Type: [number -> number]
 (define (cubicroot num) (expt num (/ 1 3)))
-(cubicroot 27)
+
+;1.b
+;Type: [number*number*number -> number]
+(define (3geomavg a b c) (cubicroot (* a b c)))
+
+;1.c
+;Type: []
+(define (numLarger a b c)
+(cond
+  ((and (> a (3geomavg a b c)) (> b (3geomavg a b c))) 2)
+  ((and (> a (3geomavg a b c)) (> c (3geomavg a b c))) 2)
+  ((and (> b (3geomavg a b c))(> c (3geomavg a b c))) 2)
+  (else 1)
+  ))
+
+(numLarger 1 4 4)
