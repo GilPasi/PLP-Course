8d0f77056ee749db90df70033a97bbdc606fbfc2
Gil Pasi <gilp@semperis.com>
2025-04-10 14:24:42 +0300

    [add]: instructions added; first question solved

---
+++ plp_assignment1.rkt
@@ -0,0 +1,7 @@
+;Programming Languages Principles - Assigment 1
+; Authors: Gil Pasi (________) | Yulia Moshan (_________)
+
+;1.a
+;Type: [number -> number]
+(define (cubicroot num) (expt num (/ 1 3)))
+(cubicroot 27)
