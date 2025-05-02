c2b505c047bc1abc2ac237222bc28d4f8863291b
Gil <gilpasi@gmail.com>
2025-05-02 12:06:41 +0300

    [add]: Function 'reverse-li' added

--- assignment-2/plp_assignment2.rkt
+++ assignment-2/plp_assignment2.rkt
@@ -8,7 +8,8 @@
 ;Preconditions: No null elements other than the tail element
 ;Tests: (make-dup-list (list 1 2 3 4)) -> (1 1 2 2 3 3 4 4)
 (define (make-dup-list li)  
-   (if (null? li)
+   {
+    if (null? li)
        li
        (let ((head (car li)) (rest (cdr li)))
         (cons
@@ -18,20 +19,37 @@
         )
       )
     )
-  )
+  }
 )
-
+(equal? (make-dup-list (list 1 2 3 4)) (list 1 1 2 2 3 3 4 4))
 ;1.b
 ;Type: [List(T) -> List(Pair(T))
 ;Preconditions: Length(list) >= 2, Length(list)|2 , No null elements other than the tail element
-;Tests: (make-list-pairs '(1 2 3 4)) -> ((1 . 2) (3 . 4))
+;Tests: (make-list-pairs (list 1 2 3 4)) -> ((1 . 2) (3 . 4))
 (define (make-list-pairs li)
-  (if (< (length li) 2) li
-      { cons 
+  {
+   if (< (length li) 2) li
+      ( cons 
               (cons (car li) (cadr li))
               (make-list-pairs (cddr li)) 
-          }
-      )
+          )
+      }
+)
+(equal? (make-list-pairs (list 1 2 3 4)) (list (cons 1 2) (cons 3 4)))
+
+;1.c
+;Type: [List(T) -> List(T)]
+;Tests
+;(reverse-li (list 1 2 3)) -> (3 2 1)
+(define (reverse-li-helper li prev)
+  {
+   if
+    (not (null? li))
+    (reverse-li-helper (cdr li) (cons (car li) prev))
+    prev
+  }
 )
 
+(define (reverse-li li) (reverse-li-helper li null))
+(equal? (reverse-li (list 1 2 3 4)) (list 4 3 2 1))
 
